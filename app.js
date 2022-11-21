e949c87d560e0132aa301dcd51287599d4046963
chester <chester0148@gmail.com>
2022-11-22 00:53:18 +0800

    add: 新增 websocket

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ var app = express();
 
 const cors = require('cors');
 
+const WebSocketServer = require('ws');
+
+const wss = new WebSocketServer.Server({ port: 3001 });
+
+wss.on("connection", ws => {
+  console.log("new client connected");
+  // sending message
+  ws.on("message", data => {
+      console.log(`Client has sent us: ${data}`)
+  });
+  // handling what to do when clients disconnects from server
+  ws.on("close", () => {
+      console.log("the client has connected");
+  });
+  // handling client connection error
+  ws.onerror = function () {
+      console.log("Some Error occurred")
+  }
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
